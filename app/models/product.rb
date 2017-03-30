3cf72fa7d44b9996fc5d3f09888e2825351d2620
Heinz Aufner <heinzaufner@hotmail.com>
2017-03-30 18:03:48 +1100

    Validation playtime

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
+  validates :title, length: {minimum: 5, too_short: "needs to be 5 characters or more guv!"}
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image'
