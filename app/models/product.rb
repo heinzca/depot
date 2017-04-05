5e72eea4766d4adce0c73a3126a3491a988328dc
Heinz Aufner <heinzaufner@hotmail.com>
2017-04-06 08:25:32 +1000

    cart creation d1-3

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -7,4 +11,13 @@ class Product < ApplicationRecord
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image'
   } # allow blank set to true to avoid multiple errors, as presence validation already there.
+
+  private
+  # ensure that there are no line items referncing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line items present')
+        throw :abort
+      end
+    end
 end
